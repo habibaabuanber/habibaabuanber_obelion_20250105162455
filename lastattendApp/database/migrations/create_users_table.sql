bb061353897e505a9e28917b147e9a339c3b4f4c
Habiba Khalid <habibaabuanber@gmail.com>
2025-01-06 17:49:17 +0200

    Update create_users_table.sql

--- lastattendApp/database/migrations/create_users_table.sql
+++ lastattendApp/database/migrations/create_users_table.sql
@@ -1,61 +1,10 @@
-const { Model, DataTypes, Sequelize } = require('sequelize');
-
-class User extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        firstName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            notEmpty: true,
-          },
-        },
-        familyName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            notEmpty: true,
-          },
-        },
-        email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-            isEmail: true,
-            notEmpty: true,
-          },
-        },
-        phoneNumber: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            notEmpty: true,
-            is: /^(\\+?\\d{1,4}|\\d{1,4})?\\s?\\d{10}$/,
-          },
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            notEmpty: true,
-          },
-        },
-      },
-      {
-        sequelize,
-        modelName: 'User',
-        timestamps: false,
-        hooks: {
-          beforeCreate: async (user) => {
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(user.password, salt);
-          },
-        },
-      }
-    );
-  }
-}
-
-module.exports = User;
+CREATE TABLE User (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  firstName VARCHAR(255) NOT NULL,
+  familyName VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  phoneNumber VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
